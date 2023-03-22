4c4a58ec3c13e183071b901620ae90a3ec4b079a
Mandy Petrakis <mandypetrakis@gmail.com>
2023-03-22 14:55:33 -0700

    commit tree c9d37b5ae052a89ffe25963d7124636aad8c1269

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+  def kind 
+     if @side_one == 0 || @side_three == 0 || @side_two == 0 || @side_one + @side_two <= @side_three || @side_three + @side_one <= @side_two || @side_three + @side_two <= @side_one then raise TriangleError
+     elsif @side_one == @side_three && @side_one == @side_two then :equilateral
+     elsif @side_one == @side_two || @side_one == @side_three || @side_three == @side_two then :isosceles
+      else :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message 
+      "This is not a triangle"
+    end
+  end
 end
